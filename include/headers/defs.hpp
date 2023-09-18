73d6ddf33c3a4edf40cb9d4ab16af5b82df46cde
Mikey <michael.j.bocelli@gmail.com>
2023-09-18 18:24:57 -0400

    template headers and hardware struct

---
+++ include/headers/defs.hpp
@@ -0,0 +1,10 @@
+#ifndef DEFS_HPP
+#define DEFS_HPP
+
+#define SCREEN_WIDTH = 1920
+#define SCREEN_HEIGHT = 1080
+
+#define SPRITE_SCALE = 1 
+#define FPS = 60
+
+#endif
---
+++ include/headers/hardware.hpp
@@ -0,0 +1,9 @@
+#ifndef HARDWARE_HPP
+#define HARDWARE_HPP
+
+struct screen {
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+};
+
+#endif
